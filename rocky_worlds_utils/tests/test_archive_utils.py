60afd1415753f876db1ffe9c333c5da3c3fa3798
Mees Fix <mfix@stsci.edu>
2025-03-27 17:17:03 -0400

    Adding second target to test

--- rocky_worlds_utils/tests/test_archive_utils.py
+++ rocky_worlds_utils/tests/test_archive_utils.py
@@ -1,12 +1,19 @@
 import numpy as np
+import pytest
 
 from rocky_worlds_utils.archive_utils import query_nexsci_archive
 
 
-def test_query_nexsci_archive():
-    planet_name = "LTT 1445 A b"
+@pytest.mark.parametrize(
+    "planet_name, ra, dec, preferred_idx",
+    [
+        ("LTT 1445 A b", 45.4624781, -16.5944956, 1),
+        ("TRAPPIST-1 g", 346.6263919, -5.0434618, 2),
+    ],
+)
+def test_query_nexsci_archive(planet_name, ra, dec, preferred_idx):
     all_planet_data, preferred_data_index = query_nexsci_archive(planet_name)
 
-    assert preferred_data_index[0] == 1
-    assert all(np.isclose(all_planet_data["ra"].value, 45.4624781))
-    assert all(np.isclose(all_planet_data["dec"].value, -16.5944956))
+    assert preferred_data_index[0] == preferred_idx
+    assert all(np.isclose(all_planet_data["ra"].value, ra))
+    assert all(np.isclose(all_planet_data["dec"].value, dec))
