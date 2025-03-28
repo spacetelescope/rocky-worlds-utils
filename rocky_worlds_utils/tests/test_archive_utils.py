b13b240efeda7a12e2cae4db44b8e8ee4198e0e1
Mees Fix <mfix@stsci.edu>
2025-03-28 10:33:14 -0400

    Adding another planet

--- rocky_worlds_utils/tests/test_archive_utils.py
+++ rocky_worlds_utils/tests/test_archive_utils.py
@@ -10,6 +10,7 @@ from rocky_worlds_utils.archive_utils import query_nexsci_archive
         ("LTT 1445 A b", 45.4624781, -16.5944956, 1),
         ("TRAPPIST-1 g", 346.6263919, -5.0434618, 2),
         ("GJ 357 b", 144.007464, -21.6650634, 2),
+        ("K2-415 b", 137.2015437, 11.8622503, 1),
     ],
 )
 def test_query_nexsci_archive(planet_name, ra, dec, preferred_idx):
