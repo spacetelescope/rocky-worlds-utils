879a296c734bf0997151b50e6d1f4e8b1d24202d
Leonardo dos Santos <ldsantos@stsci.edu>
2025-07-18 16:29:26 -0400

    Added function to download HST data

--- rocky_worlds_utils/hst/tools.py
+++ rocky_worlds_utils/hst/tools.py
@@ -8,11 +8,14 @@ Authors
 - Leonardo dos Santos <<ldsantos@stsci.edu>>
 """
 
+from astroquery.mast import Observations
+from crds import assign_bestrefs
+import glob
 import numpy as np
 
 
 __all__ = [
-    "nearest_index",
+    "nearest_index", "get_observations"
 ]
 
 
@@ -38,3 +41,41 @@ def nearest_index(array, target_value):
     right = array[index]
     index -= target_value - left < right - target_value
     return index
+
+
+# Module to download HST data
+def get_observations(dataset, save_path, download_references=False):
+    """
+    Download HST observations of a specific dataset or a list of datasets from
+    the MAST archive.
+
+    Parameters
+    ----------
+    dataset : ``str`` or ``list``
+        Observing ID or list of observing IDs to be downloaded from MAST.
+
+    save_path : ``str``
+        Location to save downloaded observations.
+
+    download_references : ``bool``, optional
+        Sets whether to download the reference files corresponding to the
+        requested observing IDs. Default is ``False``.
+    """
+    if isinstance(dataset, str):
+        obs_id = [dataset, ]
+    elif isinstance(dataset, list):
+        obs_id = dataset
+    else:
+        raise TypeError("dataset must be either str or list")
+
+    # Querying MAST for the requested dataset
+    data_query = Observations.query_criteria(obs_id=obs_id)
+    mast_products = Observations.get_product_list(data_query)
+
+    # Download the data
+    Observations.download_products(mast_products, download_dir=str(save_path),
+                                   project=["CALSTIS", "CALCOS"], flat=True)
+
+    if download_references:
+        files = glob.glob(dataset + "*")
+        assign_bestrefs(files, sync_references=True, verbosity=0)
